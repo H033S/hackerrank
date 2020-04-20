8f26703a93f375f88a511dbf78a2469576ab895a
H033S <tonito.nazco@gmail.com>
2020-04-19 23:40:31 -0400

    Array Manipulation Ready

--- Interview_Preparation_Kit/Arrays/Array Manipulation.py
+++ Interview_Preparation_Kit/Arrays/Array Manipulation.py
@@ -1,41 +1,27 @@
-global s
-global lazy
+#!/bin/python3
 
-def update(id, l, r, x):
-    if not (r - l):
-        s[id] += x
-    else:
-        lazy[id] += x
-        s[id] += x
+import math
+import os
+import random
+import re
+import sys
 
-def shift(id, l, r):
-    mid = (l + r) >> 1
+# Complete the arrayManipulation function below.
+def arrayManipulation(n, queries):
+    arr = [0]*(n+1)
 
-    update(id<<1, l, mid, lazy[id])
-    update(id<<1|1, mid + 1, r, lazy[id])
-    lazy[id] = 0
+    for a, b, v in queries:
+        arr[a] += v
 
-def increase(x, y, v, id, l, r):
-    if x > r or l > y:
-        return
-    if x <= l <= r <= y:
-        update(id, l, r, v)
-        return
+        if b + 1 <= n:
+            arr[b + 1] -= v
     
-    shift(id, l, r)
-
-    mid = (l + r) >> 1
-    increase(x, y, v, id<<1, l, mid)
-    increase(x, y, v, id<<1|1, mid + 1, r)
-    s[id] = max(s[id << 1], s[id << 1|1])
-
-
-def arrayManipulation(n, queries):    
-    for q in queries:
-        increase(q[0], q[1], q[2], 1, 0, n)    
-
-    return s[1]
-    #return query(1, n, 0, 0, n)  
+    mx , sm = 0, 0
+    for it in arr:
+        sm += it
+        mx = max(sm, mx)
+    
+    return mx
 
 if __name__ == '__main__':
     #fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -47,15 +33,12 @@ if __name__ == '__main__':
     m = int(nm[1])
 
     queries = []
-    s = [0]*(4*n)
-    lazy = [0]*(4*n)
 
     for _ in range(m):
         queries.append(list(map(int, input().rstrip().split())))
 
     result = arrayManipulation(n, queries)
     print(result)
-
     #fptr.write(str(result) + '\n')
 
     #fptr.close()
